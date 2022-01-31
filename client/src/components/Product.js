2ed287aa95e8a982a34f5cc2a3d0c9ea7d5ebc90
ip11 <iprash011@gmail.com>
2022-01-31 13:34:08 +0530

    Ratings Done]

--- client/src/components/Product.js
+++ client/src/components/Product.js
@@ -5,11 +5,13 @@ import Rating from './Rating';
 function Product({ product }) {
     return (
         <div className="col-3">
-            <img src={product.image} />
-            <h2>{product.name}</h2>
-            <h3 style={{ color: "#ff523b" }}>{product.rating} from {product.numReviews} reviews</h3>
-            <Rating rating={product.rating} />
-            <h1>${product.price}</h1>
+            <a href={`/product/${product._id}`}>
+                <img src={product.image} />
+                <h2>{product.name}</h2>
+                <h3 style={{ color: "#ff523b" }}>{product.rating} from {product.numReviews} reviews</h3>
+                <Rating rating={product.rating} />
+                <h1>${product.price}</h1>
+            </a>
         </div>
     )
 }
--- client/src/components/Rating.js
+++ client/src/components/Rating.js
@@ -3,12 +3,11 @@ import React from 'react';
 function Rating({ rating }) {
     return (
         <div className="rating" style={{ color: "#ff523b" }}>
-            <i className="fas fa-star"></i>
-            <i className="fas fa-star"></i>
-            <i className="fas fa-star"></i>
-            <i className="far fa-star-half-alt"></i>
-            <i className="far fa-star"></i>
-
+            <i className={rating >= 1 ? 'fas fa-star' : rating >= 0.5 ? 'far fa-star-half-alt' : 'far fa-star'}></i>
+            <i className={rating >= 2 ? 'fas fa-star' : rating >= 1.5 ? 'far fa-star-half-alt' : 'far fa-star'}></i>
+            <i className={rating >= 3 ? 'fas fa-star' : rating >= 2.5 ? 'far fa-star-half-alt' : 'far fa-star'}></i>
+            <i className={rating >= 4 ? 'fas fa-star' : rating >= 3.5 ? 'far fa-star-half-alt' : 'far fa-star'}></i>
+            <i className={rating >= 5 ? 'fas fa-star' : rating >= 4.5 ? 'far fa-star-half-alt' : 'far fa-star'}></i>
         </div>
     );
 }
